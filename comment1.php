5cb9c10c8690d2df8399cacddf6fe96fae49fa65
WK232 <121464524+WK232@users.noreply.github.com>
2023-05-23 21:49:54 +0100

    Add files via upload

---
+++ comment1.php
@@ -0,0 +1,12 @@
+<?php 
+        $comment=$_POST['comment'];
+        $conn = new PDO('mysql:host=localhost;dbname=test;charset=utf8','root','');
+		if (mysqli_connect_error()){
+			die('Connection Failed : '.mysqli_connect_error());
+		}else{
+			$stmt=$conn->prepare('insert into comments (nb_news,comment) 
+			values (1,:comment)');
+			$stmt->execute(array('comment'=>$comment));
+			echo "thank you for your comment";
+		}
+?>
